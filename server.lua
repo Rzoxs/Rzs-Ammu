2752f4499c683b049cc998de9eb5ef1dcbea3a0b
Rzoxs <57816758+Rzoxs@users.noreply.github.com>
2023-05-04 22:39:41 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,23 +1,25 @@
 local ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback('rzs_ammu:buy', function(source, cb, table)
+ESX.RegisterServerCallback('rzs_ammu:buy', function(source, cb, a, b)
     local xPlayer = ESX.GetPlayerFromId(source)
-    local total = Total()
-    if xPlayer.getMoney() >= total then
-        xPlayer.removeMoney(total)
-        for _,v in pairs(table) do
-            if v.type == "weapon" then
-                xPlayer.addWeapon(v.name, ConfigAmmu.Ammo)
-            elseif v.type == "item" then
-                xPlayer.addInventoryItem(v.name, v.qty)
+    local total = Total(a)
+    if b ~= nil or  then
+        if xPlayer.getMoney() >= total then
+            xPlayer.removeMoney(total)
+            for _,v in pairs(a) do
+                if v.type == "weapon" then
+                    xPlayer.addWeapon(v.name, ConfigAmmu.Ammo)
+                elseif v.type == "item" then
+                    xPlayer.addInventoryItem(v.name, v.qty)
+                end
             end
+            TriggerClientEvent("esx:showNotification", source, "Vous avez acheté toutes ce qui est dans votre panier pour ~g~"..total.." "..ConfigAmmu.money)
+            cb(true)
+        else
+            TriggerClientEvent("esx:showNotification", source, "~r~Il vous manque de l'argent pour acheter tout ce qui est dans votre panier "..total-xPlayer.getMoney().." "..ConfigAmmu.money)
+            cb(false)
         end
-        TriggerClientEvent("esx:showNotification", source, "Vous avez acheté toutes ce qui est dans votre panier pour ~g~"..total.." "..ConfigAmmu.money)
-        cb(true)
-    else
-        TriggerClientEvent("esx:showNotification", source, "~r~Il vous manque de l'argent pour acheter tout ce qui est dans votre panier "..total-xPlayer.getMoney().." "..ConfigAmmu.money)
-        cb(false)
     end
 end)
 
